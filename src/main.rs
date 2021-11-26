d38eddaa4ba6de166aae7048803f3d252f5bd844
Maneren <49210777+Maneren@users.noreply.github.com>
2021-11-26 18:41:33 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![feature(int_log)]
 #![feature(is_sorted)]
 
-// use rand::seq::SliceRandom;
+use rand::seq::SliceRandom;
 use std::time::Instant;
 
 mod array;
@@ -14,26 +14,26 @@ use sorts::{get_sorts, run_sort, Sort};
 
 type Item = usize;
 
-const ITEM_COUNT: Item = 10000;
+const ITEM_COUNT: Item = 50000;
 
 fn main() {
   let sorts_dictionary = get_sorts();
 
   let mut nums = ArrayWithCounters::new((0..ITEM_COUNT).collect());
 
-  // let mut rng = rand::thread_rng();
+  let mut rng = rand::thread_rng();
 
   macro_rules! check_sort {
     ( $sort: expr) => {{
-      // nums.shuffle(&mut rng);
-      // nums.reverse();
+      nums.shuffle(&mut rng);
+      nums.reverse();
 
       let start = Instant::now();
       run_sort(&sorts_dictionary, $sort, &mut nums);
       let time = start.elapsed();
 
       println!("{:?} - {:?}", $sort, time);
-      println!("{}", nums.poll());
+      println!("{}\n", nums.poll());
 
       if !nums.data.is_sorted() {
         for (a, b) in (0..nums.len()).zip(1..nums.len()) {
@@ -44,17 +44,16 @@ fn main() {
         panic!("Incorrect!!!");
       }
 
-      println!();
-
       nums.reset();
     }};
   }
 
-  /* check_sort!(Sort::Bubble);
+  check_sort!(Sort::Bubble);
   check_sort!(Sort::CoctailShaker);
   check_sort!(Sort::Selection);
   check_sort!(Sort::Gnome);
-  check_sort!(Sort::Insertion); */
+  check_sort!(Sort::Insertion);
+  check_sort!(Sort::Strand);
   check_sort!(Sort::Heap);
   check_sort!(Sort::Quick);
   check_sort!(Sort::InPlaceQuick);
@@ -62,8 +61,7 @@ fn main() {
   check_sort!(Sort::Intro);
   check_sort!(Sort::Merge);
   check_sort!(Sort::Tim);
-  // check_sort!(Sort::InPlaceMerge);
-  // check_sort!(Sort::WeaveMerge);
+  check_sort!(Sort::InPlaceMerge);
+  check_sort!(Sort::WeaveMerge);
   check_sort!(Sort::Counting);
-  check_sort!(Sort::Strand);
 }
